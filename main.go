1a36ac71cadacb6a2067298bec4436750cf8d0e4
bhois <bernd.hois@infonova.com>
2015-07-28 15:28:12 +0200

    [eve] introducing toml configuration file

--- main.go
+++ main.go
@@ -1,31 +1,64 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net/http"
 	"strings"
 
+	"github.com/BurntSushi/toml"
 	"github.com/Shopify/sarama"
 )
 
+const (
+	// Default hostname for http server
+	DefaultHostname = "localhost"
+
+	// Default bind address for http server
+	DefaultBindAddress = ":8080"
+
+	// Default kafka connect
+	DefaultKafkaConnect = "localhost:9092"
+)
+
 var (
-	ip                 = flag.String("ip", "0.0.0.0", "ip address")
-	port               = flag.String("port", "8080", "port")
-	kafkaConnect       = flag.String("kafka_connect", "localhost:9092", "kafka broker list")
 	asyncEventProducer sarama.AsyncProducer
 )
 
+type tomlConfig struct {
+	Title string
+	Http  httpInfo
+	Kafka kafkaInfo
+}
+
+type httpInfo struct {
+	Hostname    string
+	BindAddress string `toml:"bind_address"`
+}
+
+type kafkaInfo struct {
+	Connect string
+}
+
 func main() {
-	flag.Parse()
+	var config tomlConfig
+	if _, err := toml.DecodeFile("etc/eve.toml", &config); err != nil {
+		log.Println("No eve.toml configuration file found, taking defaults.")
+		config.Title = "EvE - Event Entrance"
+		config.Http.Hostname = DefaultHostname
+		config.Http.BindAddress = DefaultBindAddress
+		config.Kafka.Connect = DefaultKafkaConnect
+	}
+	log.Println("Starting " + config.Title)
 
 	router := NewRouter()
 
-	brokerList := strings.Split(*kafkaConnect, ",")
+	brokerList := strings.Split(config.Kafka.Connect, ",")
 
-	log.Println("Initializing kafka producer connecting to " + *kafkaConnect)
+	log.Println("Initializing kafka producer connecting to " + config.Kafka.Connect)
 	asyncEventProducer = NewAsyncEventProducer(brokerList)
 
-	log.Println("Starting http server listening on port " + *port)
-	log.Fatal(http.ListenAndServe(*ip+":"+*port, router))
+	log.Println("Starting http server " +
+		config.Http.Hostname +
+		config.Http.BindAddress)
+	log.Fatal(http.ListenAndServe(config.Http.Hostname+config.Http.BindAddress, router))
 }
